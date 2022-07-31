8b97ca894a48e52df9b568c4c26a12bd250984b6
erophey7 <erophey7@gmail.com>
2022-08-01 01:47:07 +1000

    update

--- main.py
+++ main.py
@@ -110,6 +110,11 @@ while True:
                     pass
             else:
                 os.system(f"mkdir {settings['Servers_dir']}")
+                os.system(f'cp ServerExample {settings["Servers_dir"]}/{name}')
+                if choiceCore != "2":
+                    os.system(f'wget -c {download_link} -o server.jar')
+                else:
+                    pass
 
 
 
