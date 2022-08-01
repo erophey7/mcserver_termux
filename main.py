8d07aeba3d15fc662c7b24308ba168990a4c659b
erophey7 <erophey7@gmail.com>
2022-08-01 16:39:14 +1000

    debugging

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ while True:
 
                 elif choice == '2':
                         if ftpStarted == False:
-                            os.system(f'busybox tcpsvd -vE {func.getLocalIP} {settings["FTP_port"]} busybox ftpd -w {serverDir}')
+                            subprocess.Popen('busybox' 'tcpsvd' '-vE' f'{func.getLocalIP}' f'{settings["FTP_port"]}' 'busybox' 'ftpd -w' f'{serverDir}')
                             ftpStarted = True
                         else:
                             os.sytem('pkill ftpd')
