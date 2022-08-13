9de10f0b09789de7abd506897256ad5da150ead4
erophey7 <erophey7@gmail.com>
2022-08-13 19:35:07 +1000

    debugging

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ while True:
                     break
 
                 elif choice == '1':
-                    subprocess.run([f"java", f"-Xms{settings['Xms']}", f"-Xmx{settings['Xmx']}", "-jar", f"{serverDir}/server.jar", "nogui"])
+                    subprocess.run([f"java", f"-Xms{settings['Xms']}m", f"-Xmx{settings['Xmx']}m", "-jar", f"{serverDir}/server.jar", "nogui"])
                     input()
 
                 elif choice == '2':
