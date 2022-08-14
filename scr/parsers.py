f67daad2d1dfb2ac5ddbe7703b455d5bb45a5a39
green <anonimedered@gmail.com>
2022-08-14 21:57:33 +1000

    Just update

--- scr/parsers.py
+++ scr/parsers.py
@@ -13,30 +13,38 @@ async def get(url, sess):
 
 async def request(urls):
     async with http.ClientSession() as session:
-        tasks = [asyncio.create_task(get(i, session)) for i in urls]
+        tasks = (asyncio.create_task(get(i, session)) for i in urls)
         return await asyncio.gather(*tasks)
 
-async def vanillaParser():
+
+async def vanillaParser() -> tuple:
     _SITE = "https://getbukkit.org/download/vanilla"
+
     async with http.ClientSession() as session:
         soup = bs4.BeautifulSoup(await get(_SITE, session), 'lxml')
-        download_links = [re.search(r'https://[^"]+', str(i)).group() for i in soup.select('a.btn-download')]
-        download_links = [re.search(r'https://[^"]+',str(bs4.BeautifulSoup(i, 'lxml').select("h2 a"))).group() for i in await request(download_links)]
-        version_links = [re.search(r'\d\.[^<]+', str(i)).group() for i in soup.select('div.col-sm-3 h2')]
+        download_links = (re.search(r'https://[^"]+', str(i)).group() for i in soup.select('a.btn-download'))
+        download_links = (re.search(r'https://[^"]+', str(bs4.BeautifulSoup(i, 'lxml').select("h2 a"))).group() for i in await request(download_links))
+        version_links = (re.search(r'\d\.[^<]+', str(i)).group() for i in soup.select('div.col-sm-3 h2'))
 
-        return list(zip(version_links, download_links))
+    return tuple(zip(version_links, download_links))
+
+
+async def forgeParser() -> list:
+
+    return []
 
 
 def vanilla():
     return asyncio.run(vanillaParser())
 
+
 def forge():
     return asyncio.run(forgeParser())
 
+
 # Полигон испытаний
 
-pprint.pprint(vanilla())
-
+pprint.pprint(forge())
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(levelname)-12s %(asctime)s %(message)s')
