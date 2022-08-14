c3cf5566d4cf99000b0b76c0c90ff0c42a1e7d7c
green <anonimedered@gmail.com>
2022-08-14 21:35:20 +1000

    Just update

--- scr/parsers.py
+++ scr/parsers.py
@@ -17,29 +17,21 @@ async def request(urls):
         return await asyncio.gather(*tasks)
 
 async def vanillaParser():
-    SITE = "https://getbukkit.org/download/vanilla"
+    _SITE = "https://getbukkit.org/download/vanilla"
     async with http.ClientSession() as session:
-        soup = bs4.BeautifulSoup(await get(SITE, session), 'html.parser')
+        soup = bs4.BeautifulSoup(await get(_SITE, session), 'lxml')
         download_links = [re.search(r'https://[^"]+', str(i)).group() for i in soup.select('a.btn-download')]
-
+        download_links = [re.search(r'https://[^"]+',str(bs4.BeautifulSoup(i, 'lxml').select("h2 a"))).group() for i in await request(download_links)]
         version_links = [re.search(r'\d\.[^<]+', str(i)).group() for i in soup.select('div.col-sm-3 h2')]
+
         return list(zip(version_links, download_links))
-
-# Vanilla minecraft site parser
-
-# async def vanillaParser():
-#     soup = bs4.BeautifulSoup(str(await request(['https://getbukkit.org/download/vanilla'])), 'html.parser')
-#     result = str(soup.find_all('div', class_='row vdivide'))
-#     version = re.findall(r'\d\.\d+\.*\d*', result)[::2]
-#     gacha_urls = re.findall(r'https://getbukkit.org/get/[^"]*', result)
-#     soup = bs4.BeautifulSoup(str(await request(gacha_urls)), 'html.parser')
-#     urls = re.findall(r'https://[^"]*', str(soup.find_all('h2')))
-#
-#     return list(zip(version, urls))
 
 
 def vanilla():
     return asyncio.run(vanillaParser())
+
+def forge():
+    return asyncio.run(forgeParser())
 
 # Полигон испытаний
 
