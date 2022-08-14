9c8602dca605517f5a850130f60cad3337a18fb0
erophey7 <erophey7@gmail.com>
2022-08-14 17:48:43 +1000

    pyngrok debugging

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ while True:
                         tcp_tunnel = ngrok.connect(settings['Standart_server_port'], "tcp")
                         tunnels = ngrok.get_tunnels()
                         print(tcp_tunnel)
-                        print(tunnels)
+                        print(tunnels.public_url)
                         input()
                         ngrokStarted = True
                     else:
