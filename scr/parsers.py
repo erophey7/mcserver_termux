69fcca03273929fd8e59c04e32a72ffb0c969af6
green <anonimedered@gmail.com>
2023-02-07 17:45:08 +1000

    Make spigot

--- scr/parsers.py
+++ scr/parsers.py
@@ -71,12 +71,12 @@ def vanilla(version: str = None):
 
 def forge(version: str = None):
 
-    with requests.Session() as sess:
+    with requests.Session() as session:
 
         with open("{}/data/forge.json".format(DIRPATH), "r") as f:
             forge = json.load(f)
 
-        html = sess.get(FORGE).text
+        html = session.get(FORGE).text
         soup = bs4.BeautifulSoup(html, "html.parser")
         versions = [
             re.search(r"\d+(\.\d+)+", str(i)).group()
@@ -92,7 +92,7 @@ def forge(version: str = None):
 
         tempDownloadLinks = [
             bs4.BeautifulSoup(
-                sess.get(
+                session.get(
                     "https://files.minecraftforge.net/net/minecraftforge/forge/index_{}.html".format(i)
                 ).text,
                 "html.parser",
@@ -115,47 +115,59 @@ def forge(version: str = None):
         return downloadLinks[version]
 
 
-# async def _spigotParser(version):
-#     _SITE =
-#
-#     async with http.ClientSession() as session:
-#         soup = bs4.BeautifulSoup(await get(_SITE, session), "html.parser")
-#         download_links = (
-#             re.search(r'https://[^"]+', str(i)).group()
-#             for i in soup.select("a.btn-download")
-#         )
-#         download_links = (
-#             re.search(
-#                 r'https://[^"]+',
-#                 str(bs4.BeautifulSoup(i, "html.parser").select("h2 a")),
-#             ).group()
-#             for i in await request(download_links)
-#         )
-#         versions = (
-#             re.search(r"\d\.[^<]+", str(i)).group()
-#             for i in soup.select("div.col-sm-3 h2")
-#         )
-#         result = dict(zip(versions, download_links))
-#
-#     return result[version]
-#
-#     # {version: [{version: y}, {url: z}]}
-#     # return (Start_Page)
-#
-#
-# def vanilla.json():
-#     return asyncio.run(_vanillaParser())
-#
-#
-# def forge(version):
-#     return asyncio.run(_forgeParser(version))
-#
-#
-# def spigot(version):
-#     return asyncio.run(_spigotParser(version))
+# Spigot
+
+def spigot(version: str = None):
+    with requests.Session() as session:
+
+        with open("{}/data/spigot.json".format(DIRPATH)) as f:
+            spigot = json.load(f)
+
+        html = session.get(SPIGOT).text
+        soup = bs4.BeautifulSoup(html, "html.parser")
+
+        versions = [str(i)[4:-5] for i in soup.select("div > h2")]
+
+        if version is None:
+            return versions
+
+        elif versions[0] in spigot:
+            return spigot[version]
+
+        elif version not in versions:
+            return False
+
+        tempDownloadsLinks = [
+            re.search(r'https://[^"]+', str(i)).group()
+            for i in soup.select("a.btn-download")
+        ]
+
+        downloadLinks = []
+
+        for j, i in enumerate(tempDownloadsLinks):
+            downloadLinks.append(
+                re.search(
+                    r'https://[^"]+',
+                    str(
+                        bs4.BeautifulSoup(
+                            session.get(tempDownloadsLinks[j]).text, "html.parser"
+                        ).select("h2 a")
+                    ),
+                ).group()
+            )
+
+        dumpsLinks = {versions[i]: downloadLinks[i] for i in range(len(versions))}
+
+        with open("{}/data/spigot.json".format(DIRPATH), "w") as f:
+            json.dump(dumpsLinks, f)
+
+        return dumpsLinks[version]
+
+    # {version: [{version: y}, {url: z}]}
+    # return (Start_Page)
 
 
 # Полигон испытаний
 
 if __name__ == "__main__":
-    print(forge("1.19.4"))
+    print(spigot("1.8.6"))
