5d007a2f7c4a10cae641214cd05c91fe4cf60a44
erophey7 <39767995+erophey7@users.noreply.github.com>
2023-02-16 22:54:49 +1000

    testing proot-distro

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@ termux-setup-storage
 apt update && apt upgrade -y
 apt install -y python busybox openjdk-17 wget net-tools termux-services proot resolv-conf openssl-tool screen proot-distro
 pip install -r requirements.txt
+proot-distro remove ubuntu
 proot-distro install ubuntu
 ngrok
 screen -dmS mcserver_install
 screen -S mcserver_install -X stuff "proot-distro login ubuntu --bind "${PWD}"/proot_scr:/root\n"
-screen -S mcserver_install -X stuff "chmod 777 install.sh && ./install.sh"
+screen -S mcserver_install -X stuff "chmod 777 install.sh && ./install.sh && exit\n"
 screen -S mcserver_install -X stuff "exit\n"
 screen -r mcserver_install
 rm -rf ${PREFIX}/bin/mcserver
